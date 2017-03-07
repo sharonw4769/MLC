e794455f7cc4822fc700bfd31c357e5e406d879b
sharonw4769 <sharonw4769@hstat.org>
2017-03-07 19:23:28 +0000

    add model

--- models/model.rb
+++ models/model.rb
@@ -4,7 +4,16 @@ require 'net/http'
 require 'json'
 require 'pp'
 
-url = 'http://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=b1b15e88fa797225412429c1c50c122a1'
-uri = URI(url)
-response = Net::HTTP.get(uri)
-pp JSON.parse(response)
+# url = 'http://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=b1b15e88fa797225412429c1c50c122a1'
+# uri = URI(url)
+# response = Net::HTTP.get(uri)
+# pp JSON.parse(response)
+
+def get_weather(name)
+    url = 'http://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=b1b15e88fa797225412429c1c50c122a1'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    result = JSON.parse(response)
+    result["weather"]
+end
+puts get_weather("Altstadt")
