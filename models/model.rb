1945e6a71042f059f612e9579471534b2604a5f1
jessicaw9494 <jessicaw9494@hstat.org>
2017-03-08 02:33:26 +0000

    London

--- models/model.rb
+++ models/model.rb
@@ -10,11 +10,10 @@ require 'pp'
 # pp JSON.parse(response)
 
 def get_weather(name)
-    #url = 'http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={APIKEY}'
-    url = 'http://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=b1b15e88fa797225412429c1c50c122a1'
+    url = 'http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1'
     uri = URI(url)
     response = Net::HTTP.get(uri)
     result = JSON.parse(response)
     result["weather"]
 end
-puts get_weather("Tokyo")
+puts get_weather("London")
