99f91421179c7473202e5c2930b17b60879e2f55
christianf9156 <christianf9156@hstat.org>
2017-03-08 18:52:35 +0000

    Getting link to work

--- models/model.rb
+++ models/model.rb
@@ -10,10 +10,11 @@ require 'pp'
 # pp JSON.parse(response)
 
 def get_weather(name)
-    url = 'http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1'
+    #url = 'http://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp&appid=b1b15e88fa797225412429c1c50c122a1'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=Miami,fl&appid=c64fc3bf3a17ec1edd28765eb1bf1a30'
     uri = URI(url)
     response = Net::HTTP.get(uri)
     result = JSON.parse(response)
     result["weather"]
 end
-puts get_weather("London")
+ # puts get_weather("Miami")
