19f312078220dd608fc8eb7b8c11ddb19f9604ac
sharonw4769 <sharonw4769@hstat.org>
2017-03-09 23:00:14 +0000

    add title to index

--- models/model.rb
+++ models/model.rb
@@ -5,17 +5,17 @@ require 'json'
 require 'pp'
 
 #url = 'http://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=b1b15e88fa797225412429c1c50c122a1'
-url = 'http://api.openweathermap.org/data/2.5/weather?q=Soeul,kr&appid=c64fc3bf3a17ec1edd28765eb1bf1a30'
+url = 'http://api.openweathermap.org/data/2.5/weather?q=Brooklyn,us&appid=c64fc3bf3a17ec1edd28765eb1bf1a30'
 uri = URI(url)
 response = Net::HTTP.get(uri)
 pp JSON.parse(response)
 
 def get_weather(name)
     #url = 'http://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp&appid=b1b15e88fa797225412429c1c50c122a1'
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=Soeul,kr&appid=c64fc3bf3a17ec1edd28765eb1bf1a30'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=Brooklyn,us&appid=c64fc3bf3a17ec1edd28765eb1bf1a30'
     uri = URI(url)
     response = Net::HTTP.get(uri)
     result = JSON.parse(response)
     result["weather"]
 end
-# puts get_weather("Soeul")
+puts get_weather("")
