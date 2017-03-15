9431effa9ca229c9136b40f5b33c0f480c531b58
christianf9156 <christianf9156@hstat.org>
2017-03-15 17:47:23 +0000

    Getting Temp

--- models/model.rb
+++ models/model.rb
@@ -1,32 +1,39 @@
 #http://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=b1b15e88fa797225412429c1c50c122a1
-#shotgun -p $PORT -o $IP
+# shotgun -p $PORT -o $IP
 require 'net/http'
 require 'json'
 require 'pp'
 
-# Weather
-# url = 'http://api.openweathermap.org/data/2.5/weather?q=Brooklyn,us&appid=c64fc3bf3a17ec1edd28765eb1bf1a30'
-# uri = URI(url)
-# response = Net::HTTP.get(uri)
-# weather = JSON.parse(response)
-# #pp JSON.parse(response)
-# puts weather["weather"] # puts the information for weather
-# puts weather["name"] # puts name of city 
 
-# def get_weather(city_name, country_name)
-#     url = "http://api.openweathermap.org/data/2.5/weather?q=#{city_name},#{country_name}&appid=c64fc3bf3a17ec1edd28765eb1bf1a30"
-#     uri = URI(url)
-#     response = Net::HTTP.get(uri)
-#     weather = JSON.parse(response)
-#     weather["weather"][0]["main"] 
-# end
-
-url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC"
+url = 'http://api.openweathermap.org/data/2.5/weather?q=Brooklyn,us&appid=c64fc3bf3a17ec1edd28765eb1bf1a30'
 uri = URI(url)
 response = Net::HTTP.get(uri)
-gif = JSON.parse(response)
+weather = JSON.parse(response)
 pp JSON.parse(response)
-puts gif[""]
+# puts weather["weather"] # puts the information for weather
+# puts weather["name"] # puts name of city 
+# puts weather["weather"][0]["description"] # puts description
+puts weather["weather"]["temp"].
+
+
+
+def get_weather(city_name, country_name)
+    url = "http://api.openweathermap.org/data/2.5/weather?q=#{city_name},#{country_name}&appid=c64fc3bf3a17ec1edd28765eb1bf1a30"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    weather = JSON.parse(response)
+    
+{:weather => weather["weather"][0]["main"], :description => weather["weather"][0]["description"], :temp => weather["weather"][][] }
+end
+
+
+
+# url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC"
+# uri = URI(url)
+# response = Net::HTTP.get(uri)
+# gif = JSON.parse(response)
+# pp JSON.parse(response)
+# puts gif[""]
 
 #puts get_weather("Brooklyn","us")
 
